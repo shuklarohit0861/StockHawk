d8ed7ec91194d34fce37f304300041858ccccac8
Rohit Shukla <shuklarohit086@gmail.com>
2017-01-18 11:51:00 +0530

    added accessibility

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -130,7 +130,7 @@ public class   MyStocksActivity extends AppCompatActivity implements LoaderManag
                       new String[] { input.toString() }, null);
                   if (c.getCount() != 0) {
                     Toast toast =
-                        Toast.makeText(MyStocksActivity.this, "This stock is already saved!",
+                        Toast.makeText(MyStocksActivity.this, getString(R.string.stock_is_already_saved),
                             Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
                     toast.show();
